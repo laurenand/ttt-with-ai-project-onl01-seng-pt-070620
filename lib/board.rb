c52efb4a2e8741154f0d9d7c66f82d3c7da46d80
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-19 00:42:19 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,19 @@ class Board
   attr_accessor :cells
   def initialize
     @cells = cells
+    cells.reset!
+  end
+  
+  def reset!
     
+  end
+  
+  def display 
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
 end
