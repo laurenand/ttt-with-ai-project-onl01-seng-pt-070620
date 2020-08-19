f6f9996c72b326939dc182877f22102e85682214
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-19 00:57:23 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -11,11 +11,11 @@ class Board
   end
   
   def display 
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
   
 end
